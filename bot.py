a78f13bba59fe9f8c9e71e551cd29aa8ceab865a
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 23:12:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,28 @@
+
+
 import telebot
+from deep_translator import GoogleTranslator
 
-bot = telebot.TeleBot('1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU')
 
-@bot.message_handler(func=lambda message: '|' in message.text)
+TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
+
+@bot.message_handler(func=lambda message: True)
+
 def handle_message(message):
     split_message = message.text.split('|')
-    bot.send_message(message.chat.id, split_message[1])
+translator = GoogleTranslator(source='auto', target='split_message')
+
+def translate(message):
+    try:
+        translated_text = translator.translate(message.text)
+        bot.send_message(message.chat.id, f"Translation: {translated_text}")
+    except Exception as e:
+        bot.send_message(message.chat.id, "An error occurred while translating your text. Please try again.")
 
 bot.polling()
