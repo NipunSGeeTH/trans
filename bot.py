8058582e386591fd00aad52ef2a4b2b7a07a0066
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 23:34:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import telebot
 from deep_translator import GoogleTranslator
 
-translator = GoogleTranslator(source='auto')
-supported_languages = ['en', 'si', 'fr', 'de', 'it', 'ja', 'pt', 'ru', 'zh-cn']
+translator = GoogleTranslator(source='auto', target='en')
 
 TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
 
@@ -10,26 +9,13 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate. You can also change the target language using the /language command.")
-
-@bot.message_handler(commands=['language'])
-def language(message):
-    bot.send_message(message.chat.id, "Available target languages: " + ", ".join(supported_languages))
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
 
 @bot.message_handler(func=lambda message: True)
 def translate(message):
-    if message.text.startswith('/language'):
-        return
-
     try:
-        # Get the user's target language
-        target_language = translator.detect(message.text)
-        if target_language not in supported_languages:
-            target_language = 'en'
-
-        # Translate the message
-        translated_text = translator.translate(message.text, target_language=target_language)
-        bot.send_message(message.chat.id, f"Translation ({target_language}): {translated_text}")
+        translated_text = translator.translate(message.text)
+        bot.send_message(message.chat.id, f"Translation: {translated_text}")
     except Exception as e:
         bot.send_message(message.chat.id, "An error occurred while translating your text. Please try again.")
 
