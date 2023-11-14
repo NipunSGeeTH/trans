2b7182529dc2b0cd40fc1dbcf3ffc47945512c89
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 22:57:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from deep_translator import GoogleTranslator
 
-translator = GoogleTranslator(source='auto')
+translator = GoogleTranslator(source='auto', target='en')
 
 TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
 
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate to English or specify a language code along with the text if you want to translate it to a different language.")
 
 @bot.message_handler(func=lambda message: True)
 def translate(message):
