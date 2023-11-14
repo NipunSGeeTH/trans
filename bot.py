2afb856d81bdd5accee4618e30698e7d1cbece14
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 23:22:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,13 +7,13 @@ from deep_translator import GoogleTranslator
 TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
 
 bot = telebot.TeleBot(TOKEN)
-
+split_message = "en"
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
 
 @bot.message_handler(func=lambda message: True)
-split_message=('en')
+
 def handle_message(message):
     split_message = message.text.split('|')
 translator = GoogleTranslator(source='auto', target=split_message)
