5ece314a155cc1acf9ffb450580f27d707d2adee
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 23:42:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot
+"""import telebot
 from deep_translator import GoogleTranslator
 
 translator = GoogleTranslator(source='auto', target='en')
@@ -15,6 +15,39 @@ def start(message):
 def translate(message):
     try:
         translated_text = translator.translate(message.text)
+        bot.send_message(message.chat.id, f"Translation: {translated_text}")
+    except Exception as e:
+        bot.send_message(message.chat.id, "An error occurred while translating your text. Please try again.")
+
+bot.polling()
+"""
+import telebot
+from deep_translator import GoogleTranslator
+
+translator = GoogleTranslator(source='auto')
+
+TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
+
+@bot.message_handler(func=lambda message: True)
+def translate(message):
+    try:
+        # Detect the language of the input message
+        detected_language = translator.detect(message.text)
+
+        # Translate the message to the target language
+        if detected_language == 'si':  # Sinhala language
+            translated_text = translator.translate(message.text, target='en')  # English language
+        elif detected_language == 'en':  # English language
+            translated_text = translator.translate(message.text, target='si')  # Sinhala language
+        else:
+            translated_text = translator.translate(message.text)  # Default translation
+
         bot.send_message(message.chat.id, f"Translation: {translated_text}")
     except Exception as e:
         bot.send_message(message.chat.id, "An error occurred while translating your text. Please try again.")
