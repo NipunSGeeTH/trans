d21561f82bc6ff547356fadc4e97793a85b613a5
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 23:59:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,25 +32,25 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    # Create the inline markup keyboard with two buttons
-    keyboard = types.InlineKeyboardMarkup()
-    english_button = types.InlineKeyboardButton(text="English", callback_data="en")
-    sinhala_button = types.InlineKeyboardButton(text="Sinhala", callback_data="si")
-    keyboard.add(english_button, sinhala_button)
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
 
-    bot.send_message(message.chat.id, "Select the language you want to translate to:", reply_markup=keyboard)
+@bot.message_handler(func=lambda message: True)
+def translate(message):
+    # Ask the user to select the target language
+    bot.send_message(message.chat.id, "Select the language you want to translate to:", reply_markup=get_language_selection_keyboard())
 
 @bot.callback_query_handler(func=lambda call: call.data in ["en", "si"])
-def translate(call):
+def translate_with_selected_language(call):
+    # Retrieve the selected target language
+    target_language = call.data
+
+    # Process the user's message
+    if call.message.reply_to_message is not None:
+        message = call.message.reply_to_message.text
+    else:
+        message = call.message.text
+
     try:
-        target_language = call.data  # Retrieve the selected target language
-
-        # Process the user's message
-        if call.message.reply_to_message is not None:
-            message = call.message.reply_to_message.text
-        else:
-            message = call.message.text
-
         # Translate the message to the selected target language
         translated_text = translator.translate(message, target=target_language)
 
@@ -59,4 +59,13 @@ def translate(call):
     except Exception as e:
         bot.answer_callback_query(callback_query_id=call.id, text="An error occurred while translating your text. Please try again.")
 
-bot.polling()  # Start the bot
+def get_language_selection_keyboard():
+    # Create the inline markup keyboard with two buttons
+    keyboard = types.InlineKeyboardMarkup()
+    english_button = types.InlineKeyboardButton(text="English", callback_data="en")
+    sinhala_button = types.InlineKeyboardButton(text="Sinhala", callback_data="si")
+    keyboard.add(english_button, sinhala_button)
+
+    return keyboard
+
+bot.polling()
