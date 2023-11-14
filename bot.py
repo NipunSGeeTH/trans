93b02f439bd6efbb32d0662f2cd8551e785ec89f
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-14 21:46:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
-from googletrans import Translator
+from deep_translator import GoogleTranslator
 
-translator = Translator()
+translator = GoogleTranslator(source='auto', target='en')
 
 TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
 
@@ -9,14 +9,13 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Welcome to the Google Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
 
 @bot.message_handler(func=lambda message: True)
 def translate(message):
     try:
-        source_language = message.chat.language_code
-        translated_text = translator.translate(message.text, dest="en").text
-        bot.send_message(message.chat.id, f"Translation from {source_language} to English: {translated_text}")
+        translated_text = translator.translate(message.text)
+        bot.send_message(message.chat.id, f"Translation: {translated_text}")
     except Exception as e:
         bot.send_message(message.chat.id, "An error occurred while translating your text. Please try again.")
 
