15a8b445e5833d8e5a8f48bc31541245145f6905
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-15 12:22:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,34 +32,31 @@ TOKEN = "1879931223:AAGhQi-yFWyrK9JOT3pRAtwlR8adrxGLmVU"
 
 bot = telebot.TeleBot(TOKEN)
 
+user_message = ""
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.")
 
 @bot.message_handler(func=lambda message: True)
-def translate(message):
+def save_user_message(message):
+    global user_message
+    user_message = message.text  # Save the user's message to a variable
+
     # Ask the user to select the target language
     bot.send_message(message.chat.id, "Select the language you want to translate to:", reply_markup=get_language_selection_keyboard())
 
 @bot.callback_query_handler(func=lambda call: call.data in ["en", "si"])
 def translate_with_selected_language(call):
+    global user_message
     # Retrieve the selected target language
     target_language = call.data
 
-    # Process the user's message
-    if call.message.reply_to_message is not None:
-        message = call.message.reply_to_message.text
-    else:
-        message = call.message.text
+    # Translate the user's message to the selected target language
+    translated_text = translator.translate(user_message, target=target_language)
 
-    try:
-        # Translate the message to the selected target language
-        translated_text = translator.translate(message, target=target_language)
-
-        # Send the translated text to the user
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Translation: {translated_text}")
-    except Exception as e:
-        bot.answer_callback_query(callback_query_id=call.id, text="An error occurred while translating your text. Please try again.")
+    # Send the translated text to the user
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Translation: {translated_text}")
 
 def get_language_selection_keyboard():
     # Create the inline markup keyboard with two buttons
