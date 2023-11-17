8dace25a3c5a67fa536207c0212dba0eed795675
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-17 14:19:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -142,10 +142,12 @@ bot = telebot.TeleBot(bot_token)
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_photo(message.chat.id, 'https://t.me/NsDevSpace/8')
-    
-    bot.send_message(message.chat.id, "Hi!")
+def handle_start(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_open_website = types.InlineKeyboardButton('Open Website', url='t.me/nipunsgeeth')
+    markup.add(btn_open_website)
+    bot.send_message(message.chat.id, 'Click the button to open the website:', reply_markup=markup)
+
 
     
 
@@ -180,4 +182,5 @@ def save_user_message(message):
 
     else:
         send_message(user_id, "Please join the channel first.")
+bot.polling()  # Start the bot
 
