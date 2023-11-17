623e78995bc4015727599a1bb58df0c74a079e4a
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-17 14:01:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def start(message):
     markup = types.InlineKeyboardMarkup()
     btn_open_website = types.InlineKeyboardButton('Open Website', url='t.me/nipunsgeeth')
     markup.add(btn_open_website)
-    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.",reply_markup)
+    bot.send_message(message.chat.id, "Welcome to the Deep Translate Telegram bot! Send me any text you want to translate and I will do my best to translate it for you.",reply_markup=markup)
     
     
 
