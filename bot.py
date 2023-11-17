4d1ec6f0a85cfe99506b476cb57fcae8f2000202
Sangeeth Nipun <76957918+NipunSGeeTH@users.noreply.github.com>
2023-11-17 14:25:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,7 +146,7 @@ def handle_start(message):
     markup = types.InlineKeyboardMarkup()
     btn_open_website = types.InlineKeyboardButton('Open Website', url='t.me/nipunsgeeth')
     markup.add(btn_open_website)
-    bot.send_message(message.chat.id,"https://t.me/NsDevSpace/8", caption="Welcome!", 'Click the button to open the website:', reply_markup=markup)
+    bot.send_message(message.chat.id,"https://t.me/NsDevSpace/8", caption="Welcome!",  reply_markup=markup)
 
 
     
